c643e12cee8c2f242acc278c4f65bc3c2c57309d
hieuph07735 <59309748+hieuph07735@users.noreply.github.com>
2021-09-11 15:29:27 +0700

    noi dung

--- resources/views/Client/checkout.blade.php
+++ resources/views/Client/checkout.blade.php
@@ -22,53 +22,12 @@ Thanh Toán
     <!-- Start Cart  -->
     <div class="cart-box-main">
         <div class="container">
-            <div class="row new-account-login">
-                <div class="col-sm-6 col-lg-6 mb-3">
-                    <div class="title-left">
-                        <h3>Account Login</h3>
-                    </div>
-                    <h5><a data-toggle="collapse" href="#formLogin" role="button" aria-expanded="false">Click here to Login</a></h5>
-                    <form class="mt-3 collapse review-form-box" id="formLogin">
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="InputEmail" class="mb-0">Email Address</label>
-                                <input type="email" class="form-control" id="InputEmail" placeholder="Enter Email"> </div>
-                            <div class="form-group col-md-6">
-                                <label for="InputPassword" class="mb-0">Password</label>
-                                <input type="password" class="form-control" id="InputPassword" placeholder="Password"> </div>
-                        </div>
-                        <button type="submit" class="btn hvr-hover">Login</button>
-                    </form>
-                </div>
-                <div class="col-sm-6 col-lg-6 mb-3">
-                    <div class="title-left">
-                        <h3>Create New Account</h3>
-                    </div>
-                    <h5><a data-toggle="collapse" href="#formRegister" role="button" aria-expanded="false">Click here to Register</a></h5>
-                    <form class="mt-3 collapse review-form-box" id="formRegister">
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="InputName" class="mb-0">First Name</label>
-                                <input type="text" class="form-control" id="InputName" placeholder="First Name"> </div>
-                            <div class="form-group col-md-6">
-                                <label for="InputLastname" class="mb-0">Last Name</label>
-                                <input type="text" class="form-control" id="InputLastname" placeholder="Last Name"> </div>
-                            <div class="form-group col-md-6">
-                                <label for="InputEmail1" class="mb-0">Email Address</label>
-                                <input type="email" class="form-control" id="InputEmail1" placeholder="Enter Email"> </div>
-                            <div class="form-group col-md-6">
-                                <label for="InputPassword1" class="mb-0">Password</label>
-                                <input type="password" class="form-control" id="InputPassword1" placeholder="Password"> </div>
-                        </div>
-                        <button type="submit" class="btn hvr-hover">Register</button>
-                    </form>
-                </div>
-            </div>
+        
             <div class="row">
                 <div class="col-sm-6 col-lg-6 mb-3">
                     <div class="checkout-address">
                         <div class="title-left">
-                            <h3>Billing address</h3>
+                            <h3>Thông tin người mua</h3>
                         </div>
                         <form class="needs-validation" novalidate>
                             <div class="row">
@@ -135,58 +94,7 @@ Thanh Toán
                                 <input type="checkbox" class="custom-control-input" id="save-info">
                                 <label class="custom-control-label" for="save-info">Save this information for next time</label>
                             </div>
-                            <hr class="mb-4">
-                            <div class="title"> <span>Payment</span> </div>
-                            <div class="d-block my-3">
-                                <div class="custom-control custom-radio">
-                                    <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
-                                    <label class="custom-control-label" for="credit">Credit card</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
-                                    <label class="custom-control-label" for="debit">Debit card</label>
-                                </div>
-                                <div class="custom-control custom-radio">
-                                    <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
-                                    <label class="custom-control-label" for="paypal">Paypal</label>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-3">
-                                    <label for="cc-name">Name on card</label>
-                                    <input type="text" class="form-control" id="cc-name" placeholder="" required> <small class="text-muted">Full name as displayed on card</small>
-                                    <div class="invalid-feedback"> Name on card is required </div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="cc-number">Credit card number</label>
-                                    <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                                    <div class="invalid-feedback"> Credit card number is required </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-3 mb-3">
-                                    <label for="cc-expiration">Expiration</label>
-                                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                                    <div class="invalid-feedback"> Expiration date required </div>
-                                </div>
-                                <div class="col-md-3 mb-3">
-                                    <label for="cc-expiration">CVV</label>
-                                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                                    <div class="invalid-feedback"> Security code required </div>
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <div class="payment-icon">
-                                        <ul>
-                                            <li><img class="img-fluid" src="images/payment-icon/1.png" alt=""></li>
-                                            <li><img class="img-fluid" src="images/payment-icon/2.png" alt=""></li>
-                                            <li><img class="img-fluid" src="images/payment-icon/3.png" alt=""></li>
-                                            <li><img class="img-fluid" src="images/payment-icon/5.png" alt=""></li>
-                                            <li><img class="img-fluid" src="images/payment-icon/7.png" alt=""></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr class="mb-1"> </form>
+                           
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-6 mb-3">
@@ -194,7 +102,7 @@ Thanh Toán
                         <div class="col-md-12 col-lg-12">
                             <div class="shipping-method-box">
                                 <div class="title-left">
-                                    <h3>Shipping Method</h3>
+                                    <h3>Dịch vụ giao hàng</h3>
                                 </div>
                                 <div class="mb-4">
                                     <div class="custom-control custom-radio">
@@ -214,7 +122,7 @@ Thanh Toán
                         <div class="col-md-12 col-lg-12">
                             <div class="odr-box">
                                 <div class="title-left">
-                                    <h3>Shopping cart</h3>
+                                    <h3>Giỏ hàng </h3>
                                 </div>
                                 <div class="rounded p-2 bg-light">
                                     <div class="media mb-2 border-bottom">
@@ -238,7 +146,7 @@ Thanh Toán
                         <div class="col-md-12 col-lg-12">
                             <div class="order-box">
                                 <div class="title-left">
-                                    <h3>Your order</h3>
+                                    <h3>Đơn hàng của bạn</h3>
                                 </div>
                                 <div class="d-flex">
                                     <div class="font-weight-bold">Product</div>
@@ -273,7 +181,7 @@ Thanh Toán
                                 </div>
                                 <hr> </div>
                         </div>
-                        <div class="col-12 d-flex shopping-box"> <a href="checkout.html" class="ml-auto btn hvr-hover">Place Order</a> </div>
+                        <div class="col-12 d-flex shopping-box"> <a href="checkout.html" class="ml-auto btn hvr-hover">Đặt hàng</a> </div>
                     </div>
                 </div>
             </div>
--- resources/views/backEnd/layouts/slideBar.blade.php
+++ resources/views/backEnd/layouts/slideBar.blade.php
@@ -7,15 +7,21 @@
                     <div class="sb-nav-link-icon"><i class="bi bi-clipboard-data"></i></div>
                     Thống kê
                 </a>
+                <a class="nav-link @if (Route::is('introduct.index')) active @endif"
+                    href="{{route('introduct.index')}}">
+                    <div class="sb-nav-link-icon"><i class="bi bi-telephone-fill"></i></div>
+                    Quản lí giới thiệu
+                </a>
                 <a class="nav-link  @if (Route::is('unit.index')) active @endif" href="{{ route('unit.index') }}">
-                    <div class="sb-nav-link-icon"><i class="bi bi-bookmarks"></i></div>
+                    <div class="sb-nav-link-icon"><i class="bi bi-people-fill"></i></i></div>
                     Quản lý nhà cung cấp
                 </a>
-                <a class="nav-link @if (Route::is('category.index')) active @endif" href="{{ route('category.index') }}">
+                <a class="nav-link @if (Route::is('category.index')) active @endif"
+                    href="{{ route('category.index') }}">
                     <div class="sb-nav-link-icon"><i class="bi bi-bookmarks"></i></div>
                     Quản lý danh mục
                 </a>
-                
+
                 <!-- <a class="nav-link collapsed @if (Route::is('type_product.index') || Route::is('product.index')) active @endif"  href="active" 
                 data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="true" aria-controls="collapseLayouts " id="a">
                     <div class="sb-nav-link-icon "><i class="bi bi-inboxes"></i></div>
@@ -28,20 +34,24 @@
                         <a class="nav-link @if (Route::is('product.index')) active @endif" href="{{route('product.index',['status'=>0])}}">Sản phẩm</a>
                     </nav>
                 </div> -->
-                <a class="nav-link @if (Route::is('type_product.index')) active @endif" href="{{ route('type_product.index') }}">
+                <a class="nav-link @if (Route::is('type_product.index')) active @endif"
+                    href="{{ route('type_product.index') }}">
                     <div class="sb-nav-link-icon"><i class="bi bi-cart4"></i></div>Quản lý loại sản phẩm
                 </a>
-                <a class="nav-link @if (Route::is('product.index')) active @endif" href="{{ route('product.index', ['status' => 0]) }}">
+                <a class="nav-link @if (Route::is('product.index')) active @endif"
+                    href="{{ route('product.index', ['status' => 0]) }}">
                     <div class="sb-nav-link-icon"><i class="bi bi-cart4"></i></div>Quản lý sản phẩm
                 </a>
 
-                <a class="nav-link @if (Route::is('inventory.index')) active @endif" href="{{ route('inventory.index') }}">
-                    <div class="sb-nav-link-icon"><i class="bi bi-cart4"></i></div>
+                <a class="nav-link @if (Route::is('inventory.index')) active @endif"
+                    href="{{ route('inventory.index') }}">
+                    <div class="sb-nav-link-icon"><i class=" bi-shop-window"></i></div>
                     Quản lý kho hàng
                 </a>
+
                 <a class="nav-link @if (Route::is('contact.index')) active @endif" href="{{route('contact.index')}}">
-                    <div class="sb-nav-link-icon"><i class="bi bi-cart4"></i></div>
-                  Liên hệ
+                    <div class="sb-nav-link-icon"><i class="bi bi-telephone-fill"></i></div>
+                    Liên hệ
                 </a>
             </div>
         </div>

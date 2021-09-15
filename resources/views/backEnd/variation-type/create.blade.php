26758a7c34861fc0ee4345b666840a07592349a5
hieuph07735 <59309748+hieuph07735@users.noreply.github.com>
2021-09-15 20:40:50 +0700

    thuoc tinh

--- resources/views/backEnd/variation-type/create.blade.php
+++ resources/views/backEnd/variation-type/create.blade.php
@@ -18,13 +18,13 @@
 
             <div class="card mb-4">
                 <div class="card-header">
-                    Tạo mới nhà cung cấp
+                    Tạo mới thuộc tính
                 </div>
                 <div class="card-body">
                     <form action="{{route('variation-type.store')}}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="exampleInputEmail1" class="breadcrumb-item active">Tên nhà cung cấp</label>
+                            <label for="exampleInputEmail1" class="breadcrumb-item active">Tên thuộc tính</label>
                             <input type="text" class="form-control" name="name" value="{{ old('name')}}">
                         </div>
                         <button type="submit" class="btn btn-success">Tạo</button>
